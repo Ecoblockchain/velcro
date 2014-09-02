b38ee77d872d944578962cb992635a95e2580bcc
cszang <zang@eml.cc>
2014-09-02 10:16:19 +0200

    new function ascale

---
+++ R/ascale.R
@@ -0,0 +1,14 @@
+##' Arbitrary scaling
+##'
+##' Rescale a vector to a given mean and variance
+##' @param x a numeric vector
+##' @param .mean the mean of the output
+##' @param .sd the sd of the output
+##' @return a rescaled numeric vector
+##' @export
+ascale <- function(x, .mean, .sd) {
+  xmean <- mean(x)
+  xsd <- sd(x)
+  .mean + (x - meanx) * .sd/sdx
+}
+
